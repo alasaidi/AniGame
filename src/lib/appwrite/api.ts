9c720651b4ee2d0624a54ee2df4d80ccd42b0a92
alasaidi <alasaidi55gmail.com>
2024-08-14 21:26:49 +0200

    storage and database design

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,15 +1,34 @@
 import { INewUser } from "@/types";
 import { ID } from "appwrite";
-import { account } from "../config";
+import { account, appwriteConfig, avatars, databases } from "./config";
+import { error } from "console";
 
 const CreateUserAccount = async (user: INewUser) => {
   try {
-    console.log(user);
     const newAccount = await account.create(ID.unique(), user.email, user.password, user.name);
+    if (!newAccount) throw error;
+    const avatarUrl = avatars.getInitials(user.name);
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+    return newUser;
   } catch (err) {
     console.log(err);
     return err;
   }
 };
+
+export async function saveUserToDB(user: { accountId: string; email: string; name: string; imageUrl: URL; username?: string }) {
+  try {
+    const newUser = await databases.createDocument(appwriteConfig.databaseId, appwriteConfig.userCollectionId, ID.unique(), user);
+    return newUser;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 export default CreateUserAccount;
