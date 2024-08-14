c238a5aee632eed8dfb02360470d335b71373104
alasaidi <alasaidi55gmail.com>
2024-08-14 17:27:00 +0200

    Auth functionality-Appwrite

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,15 @@
-const CreateUserAccount=async(user:)=>{
-    try{
+import { INewUser } from "@/types";
+import { ID } from "appwrite";
+import { account } from "../config";
 
-
-} 
+const CreateUserAccount = async (user: INewUser) => {
+  try {
+    console.log(user);
+    const newAccount = await account.create(ID.unique(), user.email, user.password, user.name);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
 
 export default CreateUserAccount;
