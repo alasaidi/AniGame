c34c8509214211ae2c38f120852eb9033affb8e2
alasaidi <alasaidi55gmail.com>
2024-08-24 21:22:04 +0200

    home page posts

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,14 +1,15 @@
 import Loader from "@/components/shared/Loader";
+import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutation";
+import { Models } from "appwrite";
 
 function Home() {
-  const isPostLoading = true;
-  const posts = null;
+  const { data: posts, isPending: isPostLoading, isError: isErrorPost } = useGetRecentPosts();
   return (
     <div className="flex flex-1">
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {isPostLoading && !posts ? <Loader /> : <ul></ul>}
+          {isPostLoading && !posts ? <Loader /> : <ul className="flex flex-col flex-1 gap-9 w-full">{posts?.documents.map((post: Models.Document) => <li>{post.caption}</li>)}</ul>}
         </div>
       </div>
     </div>
--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -1,16 +1,7 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  useInfiniteQuery,
-} from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from "@tanstack/react-query";
 import { INewPost } from "@/types";
 import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
-import createUserAccount, {
-  createPost,
-  signInAccount,
-  signOutAccount,
-} from "../appwrite/api";
+import createUserAccount, { createPost, getRecentPosts, signInAccount, signOutAccount } from "../appwrite/api";
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -20,8 +11,7 @@ export const useCreateUserAccount = () => {
 
 export const useSignInAccount = () => {
   return useMutation({
-    mutationFn: (user: { email: string; password: string }) =>
-      signInAccount(user),
+    mutationFn: (user: { email: string; password: string }) => signInAccount(user),
   });
 };
 
@@ -40,5 +30,11 @@ export const useCreatePost = () => {
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
       });
     },
+  });
+};
+export const useGetRecentPosts = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
   });
 };
